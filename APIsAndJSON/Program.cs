2c5c9deb704e086bda1208815626b9053cac9d1a
Robert Schofield <rschofield1992@gmail.com>
2024-04-24 09:52:50 -0400

    commit

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            int numAttemps = 0;
+
+            for(int i = 0; i <=10;i++)
+                if(numAttemps <= 10)
+            RonVSKanyeAPI.Kayne();
+            RonVSKanyeAPI.Ron();
+            Thread.Sleep(1000000);
         }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        public static void Ron()
+        {
+            HttpClient client = new HttpClient();
+            string ronUL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            string ronRES = client.GetStringAsync(ronUL).Result;
+
+
+            JArray ronQuote = JArray.Parse(ronRES);
+            Console.WriteLine($"Ron: {ronQuote[0]}");
+            Console.WriteLine();
+
+
+
+
+
+
+        }
+
+
+        public static void Kayne()
+        {
+
+            HttpClient client1 = new HttpClient();
+            string kanyeUR = "https://api.kanye.rest";
+            string kanyeRES = client1.GetStringAsync(kanyeUR).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeRES);
+            Console.WriteLine($"Kayne: {kanyeQuote["quote"]}");
+            Console.WriteLine();
+
+
+
+        }
+
+
+
+
+
     }
 }
